4376029160d14566cf22f3838b1f42f86a83358a
Paula Lopez <paula.pozuelo@songkick.com>
2016-02-08 16:14:40 +0000

    Added static pages and bootstrap for layout

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,12 @@ ActiveRecord::Schema.define(version: 20160208114857) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "sk_username"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
