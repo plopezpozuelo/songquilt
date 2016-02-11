b208d87ba1718c1b4bd2ac5069a75ac672d983da
Paula Lopez <paula.pozuelo@songkick.com>
2016-02-11 15:31:18 +0000

    Add calendar entries and events
    The website can now pull a users calendar from the Songkick API
    and display back the events they are going to / might go to.

--- app/controllers/calendar_entries_controller.rb
+++ app/controllers/calendar_entries_controller.rb
@@ -1,7 +1,7 @@
 class CalendarEntriesController < ApplicationController
   # Events will be handled by their sk_id
   def show
-    @calendar_entry = CalendarEntry.find([params[:user_id]), params[:event_id])])
+    @calendar_entry = CalendarEntry.find([params[:user_id]), params[:event_id]))
     @event = Event.find_by sk_id: @calendar_entry.event_id
   end
 
